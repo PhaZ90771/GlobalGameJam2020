bf0df41722496f6f78dbe12b35fedd7a035400ae
doctorbebo <byronrb92@gmail.com>
2020-01-31 23:47:35 -0800

    Cube Movement Complete

---
+++ Assets/Scripts/Cube_Moving_Scripts/Cube_Behavior.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Behavior : MonoBehaviour
+{
+    [Header("Use WSDA keys when testing cube movement")]
+
+    public float speed;
+    public float moveDistance;
+    public bool test;
+
+    private Vector3 pos;
+    private bool isMoving = false;
+ 
+
+
+    public enum Direction{positiveX, negativeX, positiveZ, negativeZ};
+    Direction direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = speed / 10;
+        pos = transform.position;
+    }
+
+    public void MoveCube(Direction i)
+    {
+        float time = 0;
+        Vector3 endPos;
+        switch (i)
+        {
+            case Direction.negativeX:
+                {
+                    endPos = pos - new Vector3(moveDistance, 0, 0);
+                    break;
+                }
+            case Direction.positiveX:
+                {
+                    endPos = pos + new Vector3(moveDistance, 0, 0);
+                    break;
+                };
+            case Direction.positiveZ:
+                {
+                    endPos = pos + new Vector3(0, 0, moveDistance);
+                    break;
+                }; ;
+            case Direction.negativeZ:
+                {
+                    endPos = pos - new Vector3(0, 0, moveDistance);
+                    break;
+                }; ;
+            default:
+                {
+                    Debug.Log("Direction for cube not assigned");
+                    return;
+                };
+        }
+        if(!isMoving)
+        {
+            StartCoroutine(Move(endPos));
+            isMoving = true;
+        }
+    }
+
+    IEnumerator Move(Vector3 endPos)
+    {
+        float time = 0;
+        while (endPos != transform.position)
+        {
+            transform.position = Vector3.Lerp(pos, endPos, time);
+            time += speed * Time.deltaTime;
+            Debug.Log(time);
+            yield return null;
+        }
+        pos = transform.position;
+        isMoving = false;
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Cube_Moving_Scripts/Trigger_Wall_3.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Wall_3 : MonoBehaviour
+{
+
+    public Cube_Behavior cube_Behavior;
+
+    Cube_Behavior.Direction dir = Cube_Behavior.Direction.negativeX;
+
+    bool test;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        test = cube_Behavior.test;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A) && test)
+        {
+            testMovment();
+            Debug.Log("Test Started");
+        }
+    }
+    // moves cube based on direction given
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("canMoveCube"))
+        {
+            cube_Behavior.MoveCube(dir);
+        }
+    }
+    void testMovment()
+    {
+        cube_Behavior.MoveCube(dir);
+
+    }
+}
