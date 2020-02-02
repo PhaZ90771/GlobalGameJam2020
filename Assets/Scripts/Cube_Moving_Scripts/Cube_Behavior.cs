60146af634191e265773bd650ef2cf13ee1890ac
Byron Brown <byronrb92@gmail.com>
2020-02-01 19:13:16 -0800

    First Level Scene

--- Assets/Scripts/Cube_Moving_Scripts/Cube_Behavior.cs
+++ Assets/Scripts/Cube_Moving_Scripts/Cube_Behavior.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Cube_Behavior : MonoBehaviour
 {
-    public float Speed;
-    public float MoveDistance;
+    public float speed;
 
     private bool isMoving = false;
     private Rigidbody rb;
@@ -37,23 +36,23 @@ public class Cube_Behavior : MonoBehaviour
             {
                 case Direction.NegativeX:
                     {
-                        rb.AddForce(new Vector3(1.033f, 0, 0), ForceMode.VelocityChange);
+                        rb.AddForce(new Vector3(1.033f, 0, 0) * speed, ForceMode.VelocityChange);
                         break;
                     }
                 case Direction.PositiveX:
                     {
-                        rb.AddForce(new Vector3(-1.033f, 0, 0), ForceMode.VelocityChange);
+                        rb.AddForce(new Vector3(-1.033f, 0, 0) * speed, ForceMode.VelocityChange);
                         break;
                     };
                 case Direction.PositiveZ:
                     {
-                        rb.AddForce(new Vector3(0, 0, 1.033f), ForceMode.VelocityChange);
+                        rb.AddForce(new Vector3(0, 0, 1.033f) * speed, ForceMode.VelocityChange);
 
                         break;
                     }; ;
                 case Direction.NegativeZ:
                     {
-                        rb.AddForce(new Vector3(0, 0, -1.033f), ForceMode.VelocityChange);
+                        rb.AddForce(new Vector3(0, 0, -1.033f) * speed, ForceMode.VelocityChange);
                         break;
                     }; ;
                 default:
