c0fa26e351f7a79efaa6712f264b7ab8e808e3c2
Benjamin Shutt <shuttben@gmail.com>
2020-02-01 22:57:32 -0800

    Show elements you have acquired

---
+++ Assets/Scripts/UI/ElementsUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementsUI : MonoBehaviour
+{
+    public Image Air;
+    public Image Fire;
+    public Image Earth;
+    public Image Water;
+
+    PlayerCharacterController playerCharacterController;
+
+    private void Awake()
+    {
+        playerCharacterController = FindObjectOfType<PlayerCharacterController>();
+    }
+
+    void Update()
+    {
+        Air.enabled = playerCharacterController.HasElement(PlayerCharacterController.ELEMENTS.AIR);
+        Fire.enabled = playerCharacterController.HasElement(PlayerCharacterController.ELEMENTS.FIRE);
+        Earth.enabled = playerCharacterController.HasElement(PlayerCharacterController.ELEMENTS.EARTH);
+        Water.enabled = playerCharacterController.HasElement(PlayerCharacterController.ELEMENTS.WATER);
+    }
+}
