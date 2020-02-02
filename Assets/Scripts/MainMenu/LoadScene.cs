c7b9c3ceeb54847b69a3d3d3d23b343653bcf989
Benjamin Shutt <shuttben@gmail.com>
2020-02-01 20:03:47 -0800

    Setup main menu to load level one when you press the play button

---
+++ Assets/Scripts/MainMenu/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string NextSceneName;
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(NextSceneName);
+    }
+}
