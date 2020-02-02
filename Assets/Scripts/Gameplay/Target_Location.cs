daf08257362439f59ce25761c6b06bc714102233
Byron Brown <byronrb92@gmail.com>
2020-02-01 22:14:44 -0800

    Level One Air Layout

--- Assets/Scripts/Gameplay/Target_Location.cs
+++ Assets/Scripts/Gameplay/Target_Location.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Target_Location : MonoBehaviour
 {
     public bool trigger = false;
+
+    public Material noKeyMat;
+    public Material keyDeliveredMat;
+
+    private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        rend.material = noKeyMat;
     }
 
     // Update is called once per frame
@@ -21,6 +27,7 @@ public class Target_Location : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Key"))
         {
+            rend.material = keyDeliveredMat;
             Debug.Log("Key Delivered");
             trigger = true;
         }
